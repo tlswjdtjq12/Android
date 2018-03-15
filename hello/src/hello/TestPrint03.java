7144b251a09bd31304aa67cdb7351afa7e9a231c
User <User@DESKTOP-H9132I5>
2018-03-15 23:23:14 +0900

    java hello world

---
+++ hello/src/hello/TestPrint03.java
@@ -0,0 +1,17 @@
+package hello;
+
+import java.util.Scanner;
+
+public class TestPrint03 {
+
+	public static void main(String[] args) {
+		
+		System.out.print("Hellow World" );
+		
+		
+		
+		
+		
+	}
+
+}
